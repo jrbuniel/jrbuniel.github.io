f56d8786155c2e0f4aab5de5042e95d0f031b97a
jrbuniel <104114225+jrbuniel@users.noreply.github.com>
2023-11-26 02:24:39 +0800

    Minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { faClipboard } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   faClipboard = faClipboard;
