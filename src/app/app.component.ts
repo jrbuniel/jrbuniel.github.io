a2f5b4cede2277618391067b109c050c706fddc1
jrbuniel <104114225+jrbuniel@users.noreply.github.com>
2024-02-07 20:46:47 +0800

    Added valentines

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Renderer2, Inject } from '@angular/core';
 import { faClipboard } from '@fortawesome/free-solid-svg-icons';
 import { DOCUMENT } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -17,11 +18,25 @@ export class AppComponent {
       
   }
 
-  click(): void {
-    this.open = !this.open;
+  // click(): void {
+  //   this.open = !this.open;
 
-    if (this.open) {
+  //   if (this.open) {
 
-    }
+  //   }
+  // }
+
+  nextPage(): void {
+    window.location.href = "yes";
+  }
+
+  moveButton(): void {
+    var noButton = document.getElementById('noButton') as HTMLElement;
+
+    var x = Math.random() * (window.innerWidth - noButton.offsetWidth) - 85;
+    var y = Math.random() * (window.innerHeight - noButton.offsetHeight) - 48;
+
+    noButton.style.left = `${x}px`;
+    noButton.style.top = `${y}px`;
   }
 }
